c338e3104ce10ced6ff483cd6db97b969d52eee9
HakureiReimuu <2494907382@qq.com>
2023-08-01 15:52:19 +0800

    Add request header disguise

--- src/main.tsx
+++ src/main.tsx
@@ -12,6 +12,10 @@ import 'primeicons/primeicons.css';
 import '@icon-park/react/styles/index.css';
 
 axios.defaults.baseURL = 'https://zonai.skland.com';
+axios.defaults.headers.platform = 1;
+axios.defaults.headers.language = 'zh-cn';
+axios.defaults.headers.vName = '0.1.4';
+axios.defaults.headers.manufacturer = 'Samsung';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <HashRouter>
